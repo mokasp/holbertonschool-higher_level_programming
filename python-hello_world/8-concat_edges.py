69368c3a51566b74f4a119adfb36fc3a5259d30c
mokasp <kasperbouldin@gmail.com>
2023-09-05 21:37:14 -0500

    more formatting and concatting

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = f"{str}"[39:66] + f"{str}"[106:112] + f"{str}"[0:6]
+print(str)
