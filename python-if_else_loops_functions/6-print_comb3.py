987100518e7f2916ae1c087bafc0cca8ae140aac
mokasp <kasperbouldin@gmail.com>
2023-09-06 22:41:22 -0500

    multiple loops and comparing with range

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for digitOne in range(10):
+    for digitTwo in range(10):
+        if digitOne != 8 and digitTwo != 9:
+            if digitOne != digitTwo and digitOne < digitTwo:
+                print("{}{}".format(digitOne, digitTwo), end=", ")
+print(89)
