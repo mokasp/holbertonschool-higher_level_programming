e49d08e56ff40ab197172b12da4ff53ffdfbcbc2
mokasp <kasperbouldin@gmail.com>
2023-09-07 04:06:25 -0500

    moved import statement

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))#!/usr/bin/python3
 def main():
     pass
 
 
 if __name__ == "__main__":
-    from add_0 import add
     a = 1
     b = 2
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
