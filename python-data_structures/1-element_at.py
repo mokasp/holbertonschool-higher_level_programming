fdeb72aeb15017ac6565d04f7b5f3d152a81ca78
mokasp <kasperbouldin@gmail.com>
2023-09-08 14:28:30 -0500

    finding an element at certain index

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
+        return None
+    else:
+        return my_list[idx]
+
+
+if __name__ == "__element_at__":
+    element_at()
