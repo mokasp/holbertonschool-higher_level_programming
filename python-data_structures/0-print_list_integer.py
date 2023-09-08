a77d3cdceb41ba106adc0d7f8a7d14873b5a074a
mokasp <kasperbouldin@gmail.com>
2023-09-08 14:07:01 -0500

    decimal formatting!!

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for number in my_list:
-        print("{}".format(number))
+        print("{:d}".format(number))
+
 
 if __name__ == "__print_list_integer__":
     print_list_integer()
