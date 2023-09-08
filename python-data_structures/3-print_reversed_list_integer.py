e5e72d63676c92252cc25c4c9042a43dff62cbeb
mokasp <kasperbouldin@gmail.com>
2023-09-08 15:30:14 -0500

    reversing list :)

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    for number in my_list[::-1]:
+        print("{:d}".format(number))
+
+
+if __name__ == "__print_reversed_list_integer__":
+    print_reversed_list_integer()
