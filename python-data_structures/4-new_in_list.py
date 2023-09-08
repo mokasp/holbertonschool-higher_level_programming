be9d3c2acf226627460a501af094b730ef45d8c8
mokasp <kasperbouldin@gmail.com>
2023-09-08 16:20:53 -0500

    making copies!

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx < 0 or idx >= len(new_list):
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
+
+
+if __name__ == "__new_in_list__":
+    new_in_list()
