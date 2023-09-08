9b9ba8e978b123784aafa2a83a25e6f8996f70a0
mokasp <kasperbouldin@gmail.com>
2023-09-08 17:39:25 -0500

    printing list of list!

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for sublist in matrix:
+        for item in sublist:
+            print("{:d}".format(item), end=" ")
+        print("")
