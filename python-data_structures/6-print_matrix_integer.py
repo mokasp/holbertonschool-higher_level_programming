94aba8d5f532b3404c04f33e19120ef8af7be97e
mokasp <kasperbouldin@gmail.com>
2023-09-08 18:07:45 -0500

    formatting error and making sure func doesnt run on import

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for sublist in matrix:
+        length = len(sublist)
+        i = 0
         for item in sublist:
-            print("{:d}".format(item), end=" ")
-        print("")
+            i += 1
+            if i < length:
+                print("{:d}".format(item), end=" ")
+            else:
+                print("{:d}".format(item))
+
+
+if __name__ == "__print_matrix_integer__":
+    print_matrix_integer()
