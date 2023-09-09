b82d9f48a2dba59cced6d680464db1c427c0d466
mokasp <kasperbouldin@gmail.com>
2023-09-08 19:42:44 -0500

    sorting and finding max int

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == "":
+        return None
+    else:
+        maximum = my_list[0]
+        for number in my_list:
+            if number > maximum:
+                maximum = number
+        return maximum
+
+
+if __name__ == "__max_integer__":
+    max_integer()
