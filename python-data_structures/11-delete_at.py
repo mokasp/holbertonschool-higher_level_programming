ed1c8b42e44805677f97f5b292a0662094c14ded
mokasp <kasperbouldin@gmail.com>
2023-09-08 22:09:18 -0500

    changed > to >= again

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     else:
         del my_list[idx]
@@ -9,4 +9,3 @@ def delete_at(my_list=[], idx=0):
 
 if __name__ == "__delete_at__":
     delete_at()
-    
