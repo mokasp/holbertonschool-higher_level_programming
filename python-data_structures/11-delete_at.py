eddbac44bc5d40b73fdf7025a5f98a8674c93034
mokasp <kasperbouldin@gmail.com>
2023-09-08 22:06:06 -0500

    using delete keyword!!

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
+
+
+if __name__ == "__delete_at__":
+    delete_at()
+    
