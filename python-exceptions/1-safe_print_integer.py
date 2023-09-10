edab7bcfb1fe317629ff9106767202bd4b31f61b
mokasp <kasperbouldin@gmail.com>
2023-09-10 15:37:02 -0500

    testing out more exceptions for checks

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -5,6 +5,8 @@ def safe_print_integer(value):
         return True
     except ValueError:
         return False
+    except TypeError:
+        return False
 
 
 if __name__ == "__safe_print_integer__":
