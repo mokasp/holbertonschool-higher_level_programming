61aca51edb91b87315849f90a935bd744b6a7b06
mokasp <kasperbouldin@gmail.com>
2023-09-10 15:06:01 -0500

    added new variable to use to measure iteration

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     for number in my_list:
         if number <= x:
+            i += 1
             print("{:d}".format(number), end="")
     print("")
     try:
-        printed = number
+        printed = i
     except TypeError:
         printed = x
     return printed
