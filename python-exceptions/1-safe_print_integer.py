2a7799ced034f43125ade220fa751ac9a2f6311c
mokasp <kasperbouldin@gmail.com>
2023-09-10 15:30:27 -0500

    value error !

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
+
+
+if __name__ == "__safe_print_integer__":
+    safe_print_integer()
