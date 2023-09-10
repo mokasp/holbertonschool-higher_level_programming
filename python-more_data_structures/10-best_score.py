bf3cfc6f38d3dc1c7dad9a20f6c0bdec33182749
mokasp <kasperbouldin@gmail.com>
2023-09-09 22:59:36 -0500

    using max and get with dictionaries

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        return max(a_dictionary, key=a_dictionary.get)
+
+
+if __name__ == "__best_score__":
+    best_score()
