a7f2d3e92262bf85c3a57eb258677b047b348b7d
mokasp <kasperbouldin@gmail.com>
2023-09-11 16:45:52 -0500

    last task!!

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            number = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            number = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            number = 0
+        except IndexError:
+            print("out of range")
+            number = 0
+        finally:
+            new_list.append(number)
+    return new_list
+
+
+if __name__ == "__list_division__":
+    list_division()
