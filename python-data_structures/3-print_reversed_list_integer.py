543fc657e7c3d62cf630ac9daeab3cb22a6b982b
mokasp <kasperbouldin@gmail.com>
2023-09-11 15:14:37 -0500

    checking if list is actually a list

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for number in list(reversed(my_list)):
-        print("{:d}".format(number))
+    if type(my_list) == list:
+        for number in reversed(my_list):
+            print("{:d}".format(number))
 
 
 if __name__ == "__print_reversed_list_integer__":
