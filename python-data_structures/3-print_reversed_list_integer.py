cb6c839809f6bb49a626f5e8399cb2a2951630e5
mokasp <kasperbouldin@gmail.com>
2023-09-11 15:11:25 -0500

    making sure reversed list is a list

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    new_list = my_list.copy()
-    for number in reversed(new_list):
+    for number in list(reversed(my_list)):
         print("{:d}".format(number))
 
 
