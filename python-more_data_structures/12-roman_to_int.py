e5a2752824a9b22d77de9f2db742f5c85b5eb010
mokasp <kasperbouldin@gmail.com>
2023-09-12 17:34:32 -0500

    more error handlign

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or len(roman_string) == 0:
+    if roman_string is None:
+        return 0
+    if type(roman_string) is not str:
         return 0
     diction = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     new_list = []
