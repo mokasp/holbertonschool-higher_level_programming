00a56281a0e17943f22257a1fded68931580b1dc
mokasp <kasperbouldin@gmail.com>
2023-09-12 16:19:58 -0500

    converting roman numerals...

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    diction = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    new_list = []
+    result = 0
+    for x in range(len(roman_string)):
+        new_list.append(diction.get(roman_string[x]))
+    for y in range(len(new_list)):
+        if y < len(new_list) - 1:
+            if new_list[y] >= new_list[y + 1]:
+                result += new_list[y]
+            else:
+                result -= new_list[y]
+    result += new_list[y]
+    return result
+
+
+if __name__ == "__roman_to_int__":
+    roman_to_int()
