340695e479ebbc25c83a13c4986d645a03e41683
mokasp <kasperbouldin@gmail.com>
2023-09-12 17:29:58 -0500

    error handling

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None or len(roman_string) == 0:
+        return 0
     diction = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     new_list = []
     result = 0
