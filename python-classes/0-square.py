e9080b448b03cde41b2c2b960126cfc7edcd7a20
mokasp <kasperbouldin@gmail.com>
2023-09-16 17:32:06 -0500

    removed extra spaces

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/bin/usr/python3
-""" This module contains a class Square. """
+"""This module contains a class Square."""
 
 
 class Square:
-    """ This Class represents an empty Square. """
+    """This Class represents an empty Square."""
 
     pass
