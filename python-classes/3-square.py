69d0680094b670bea91bc4cde4335a3586939e35
mokasp <kasperbouldin@gmail.com>
2023-09-16 21:28:34 -0500

    fixing pycodestyle

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,7 +9,7 @@ class Square:
         ----------
         size : int, optional
             size of Square
-        
+
         methods
         ----------
         area():
@@ -24,15 +24,15 @@ class Square:
         size : int, optional
             size of Square
         """
+
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-        
 
     def area(self):
         """ return area of Square """
+
         return self.__size * self.__size
-        
