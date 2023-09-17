a22961a1a9a3008925ec5daccc9ef966d96a6bf5
mokasp <kasperbouldin@gmail.com>
2023-09-16 21:24:21 -0500

    doing method npoe!

---
+++ python-classes/3-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" a module containing a class Square """
+
+
+class Square:
+    """ a class representing an empty Square
+
+        attributes
+        ----------
+        size : int, optional
+            size of Square
+        
+        methods
+        ----------
+        area():
+            returns current area of Square
+    """
+
+    def __init__(self, size=0):
+        """ constructs all attributes for a Square
+
+        parameters
+        ----------
+        size : int, optional
+            size of Square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+        
+
+    def area(self):
+        """ return area of Square """
+        return self.__size * self.__size
+        
