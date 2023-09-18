64959dfe99af54376b5da9cddbc128a2c8eb8e5f
mokasp <kasperbouldin@gmail.com>
2023-09-17 20:01:59 -0500

    trying method of making attribute public in init

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -25,8 +25,8 @@ class Square:
             size of Square
         """
 
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
