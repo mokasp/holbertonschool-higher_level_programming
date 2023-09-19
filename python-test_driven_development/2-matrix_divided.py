b122414f239463c5a8159cf37ee6598d12a5e4f2
mokasp <kasperbouldin@gmail.com>
2023-09-19 16:50:46 -0500

    type checking div

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ module containting function to dive each element of matrix """
 def matrix_divided(matrix, div):
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
     for aList in matrix:
         if type(aList) is not list:
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
