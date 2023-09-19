21b5ee2f14fc4e5be3c7c6d88381b74e8029466d
mokasp <kasperbouldin@gmail.com>
2023-09-19 15:39:02 -0500

    initial file upload for test driven development

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" module to add two integers
+
+    functions
+    ----------
+    add_integer : returns sum of two integers """
+
+
+def add_integer(a, b=98):
+    """
+    adds two integers
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    elif type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
