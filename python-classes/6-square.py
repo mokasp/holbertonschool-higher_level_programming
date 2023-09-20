ae8863bc3629e2569201fce40bed1bd552e9cdae
mokasp <kasperbouldin@gmail.com>
2023-09-19 23:19:33 -0500

    some edge cases for position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -61,7 +61,11 @@ class Square:
     def position(self, value):
         """ sets new value for private attribute position of Square """
 
-        if type(value) is not tuple or value[0] < 0 or value[1] < 0:
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
