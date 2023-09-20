9e6bd77d07b105458340d09919fb1b4f6eb725b9
mokasp <kasperbouldin@gmail.com>
2023-09-19 20:58:04 -0500

    that was TOO easy...

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,9 +6,33 @@ max_integer = __import__("6-max_integer").max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def test_max(self):
+    def test_maxend(self):
         list = [3, 4, 5]
         self.assertEqual(max_integer(list), 5)
+    
+    def test_maxbegin(self):
+        list = [5, 3, 4]
+        self.assertEqual(max_integer(list), 5)
+    
+    def test_maxmiddle(self):
+        list = [3, 5, 4]
+        self.assertEqual(max_integer(list), 5)
+    
+    def test_maxneg(self):
+        list = [3, -4, 5]
+        self.assertEqual(max_integer(list), 5)
+    
+    def test_maxend(self):
+        list = [-3, -4, -5]
+        self.assertEqual(max_integer(list), -3)
+    
+    def test_maxone(self):
+        list = [3]
+        self.assertEqual(max_integer(list), 3)
+    
+    def test_maxempty(self):
+        list = []
+        self.assertEqual(max_integer(list), None)
 
 
 if __name__ == "__main__":
