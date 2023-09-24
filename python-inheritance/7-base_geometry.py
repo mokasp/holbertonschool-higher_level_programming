dd5f8007379340b8474087975614011314f21935
mokasp <125315163+mokasp@users.noreply.github.com>
2023-09-24 23:40:04 +0000

    undo everythin -_-

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -20,4 +20,3 @@ class BaseGeometry():
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        return True
