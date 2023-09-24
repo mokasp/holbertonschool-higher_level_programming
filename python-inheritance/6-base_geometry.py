db0b2e157a5aa169bb97d9b2b209545fca14435f
mokasp <kasperbouldin@gmail.com>
2023-09-23 22:02:15 -0500

    raising exceptions in classes

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" module containing a class BaseGeometry """
+
+
+class BaseGeometry():
+    """ a class used to represent BaseGeometry
+
+    methods
+    ---------------
+    area : returns area of instance
+    """
+
+    def area(self):
+        """ returns area of an instance """
+        raise Exception("area() is not implemeted")
