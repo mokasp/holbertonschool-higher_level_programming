fda0d696dd553d4cf98e7bc7eddb0f084c7239b9
mokasp <125315163+mokasp@users.noreply.github.com>
2023-09-24 22:35:40 +0000

    pycode style

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,6 +7,5 @@ def inherits_from(obj, a_class):
     """ returns True if obj is an instance of a class that inherited \
         (directly or indirectly) from the specified class, otherwise returns \
         False """
-    if type(obj) != a_class:
+    if type(obj) is not a_class:
         return issubclass(type(obj), a_class)
-        
