aa511393bcb589adc25d6b14e901215b842b215f
mokasp <125315163+mokasp@users.noreply.github.com>
2023-09-25 01:20:00 +0000

    square using super!!!

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" module containing a class Square """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ a class used to represent a Square inherited from Rectangle
+
+    parameters
+    ==========
+    size : int
+        size of one side of Square
+
+    methods
+    =======
+    area : int
+        finds area of Square
+    """
+    def __init__(self, size):
+        """ initializes the Square """
+        super(Square, self).__init__(size, size)
