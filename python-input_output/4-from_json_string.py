9fae9a9b36951f26ac935a274a9f0d7a598644fe
mokasp <125315163+mokasp@users.noreply.github.com>
2023-09-25 06:32:58 +0000

    moved main file

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" module containing function that converts a string object to JSON """
+""" module containing function that converts JSON string to object """
 import json
 
 
 def from_json_string(my_str):
-    """ function that converts string object to JSON
+    """ function that converts JSON string to object
 
     parameters
     ==========
-    my_str : string
-        not really sure yet
+    my_str : JSON string
+        json string to convert to object
     """
     return json.loads(my_str)
