208a0d165a59590d8bfc74ee877c00f40a6f82e1
mokasp <125315163+mokasp@users.noreply.github.com>
2023-09-25 02:00:55 +0000

    need a break

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" module containing a class Square """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ a class used to represent a Square inherited from Rectangle
+
+    parameters
+    ==========
+    size : int
+        size of one side of Square
+
+    methods
+    =======
+    area : int
+        finds area of Square
+    """
+    def __init__(self, size):
+        """ initializes the Square """
+        Rectangle.__init__(self, size, size)
+    
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
