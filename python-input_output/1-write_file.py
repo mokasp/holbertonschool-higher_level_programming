974948c0da13e97c139af7aca1d5d74edba43687
mokasp <125315163+mokasp@users.noreply.github.com>
2023-09-25 06:10:16 +0000

    documenting

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,15 @@ returns the number of characters written """
 
 
 def write_file(filename="", text=""):
+    """ function that writes text to a file and returns number of charcaters \
+    written
+
+    parameters
+    ==========
+    filename : string
+        string containing a name of a file to write to
+    text : string
+        string containg text to write to file
+        """
     with open(filename, "w", encoding="UTF-8") as f:
         return f.write(text)
