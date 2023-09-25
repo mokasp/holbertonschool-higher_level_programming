c2ebf0bdfda2906688dd85f5eb51da2690adc748
mokasp <125315163+mokasp@users.noreply.github.com>
2023-09-25 05:38:12 +0000

    other print method

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,9 +10,5 @@ def read_file(filename=""):
         filename : string
             a string containing name of a textfile
     """
-    if filename == "":
-        pass
-    else:
-        with open(filename, "r", encoding="utf-8") as f:
-            read_data = f.read()
-        print(read_data)
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read())
