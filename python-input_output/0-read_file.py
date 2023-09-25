ea1819f3b77be9ea35fa6513d40577a243fea909
mokasp <125315163+mokasp@users.noreply.github.com>
2023-09-25 05:19:29 +0000

    opening and with keyword :3

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" module containing function that reads text file and prints to stdout """
+
+
+def read_file(filename=""):
+    """ reads and prints to stdout a text file
+
+        parameters
+        ==========
+        filename : string
+            a string containing name of a textfile
+    """
+    with open(filename) as f:
+        read_data = f.read()
+    print("{}".format(read_data))
