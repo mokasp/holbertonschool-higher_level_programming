8916e6b1a2a4986ba11b9cc132f3c8308ea17c0f
mokasp <kasperbouldin@gmail.com>
2023-09-25 23:45:53 -0500

    does it work???

---
+++ python-input_output/11-student.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+""" module containing a Class used to represent a Student """
+
+
+class Student():
+    """ class representing a Student
+
+    attributes
+    ==========
+    first_name : string
+        first name of student
+    last_name : string
+        last name of student
+    age : int
+        age of student
+
+    methods
+    =======
+    to_json : none
+        returns dict representation of a student instance
+    """
+    def __init__(self, first_name, last_name, age):
+        """ initializes class Student
+
+        parameters
+        ==========
+        first_name : string
+            first name of student
+        last_name : string
+            last name of student
+        age : int
+            age of student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ public instance method that returns the dictionary description \
+        for JSON serialization of an object
+
+        parameters
+        ==========
+        attrs : list
+            list of attributes to print as dict
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for i in range(len(attrs)):
+                try:
+                    new_dict[attrs[i]] = self.__dict__[attrs[i]]
+                except KeyError:
+                    pass
+            return new_dict
+
+    def reload_from_json(self, json):
+        """ public method that replaces all attributes of the Student \
+            instance
+
+            parameters
+            ==========
+            json : dict
+                dict to replace attributes from """
+
+        the_keys = list(json.keys())
+        self.first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.age = json['age']
