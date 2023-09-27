34efb08982a50194af869658b0661c5b4ac99a89
mokasp <kasperbouldin@gmail.com>
2023-09-27 17:27:22 -0500

    pascals triangle!!!!

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" module containg a function that returns a matrix representation of \
+    pascals triangle"""
+
+
+def pascal_triangle(n):
+    """ function that returns a matrix representation of \
+    pascals triangle"""
+    solved = []
+    for row in range(n):
+        new_row = [1]
+        for col in range(1, row + 1):
+            new_cell = new_row[col - 1] * float(row + 1 - col) / col
+            new_row.append(int(new_cell))
+        solved .append(new_row)
+    return solved
