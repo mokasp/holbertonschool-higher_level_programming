613d31f06a1246aa9f2b98bc6e71bbea8966d642
mokasp <kasperbouldin@gmail.com>
2023-09-27 17:34:15 -0500

    empty list

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,13 +7,13 @@ def pascal_triangle(n):
     """ function that returns a matrix representation of \
     pascals triangle"""
     solved = []
+    solved = []
     if n <= 0:
-        return solved
-    else:
-        for row in range(n):
-            new_row = [1]
-            for col in range(1, row + 1):
-                new_cell = new_row[col - 1] * float(row + 1 - col) / col
-                new_row.append(int(new_cell))
-            solved .append(new_row)
-        return solved
+        return [[]]
+    for row in range(n):
+        new_row = [1]
+        for col in range(1, row + 1):
+            new_cell = new_row[col - 1] * float(row + 1 - col) / col
+            new_row.append(int(new_cell))
+        solved .append(new_row)
+    return solved
