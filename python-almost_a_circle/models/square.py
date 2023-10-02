fbc4b9c813bba506769c9cdca75cecfe1cfa825e
mokasp <kasperbouldin@gmail.com>
2023-10-01 21:55:08 -0500

    change string formatting

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -48,9 +48,8 @@ class Square(Rectangle):
             identity of Square
         """
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     def __str__(self):
         """ returns string representation of Square """
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
-            self.id, self.x, self.y, self.size)
+        return "[{}] ({:d}) {:d}/{:d} - {:d}".format(
+            self.__class__.__name__, self.id, self.x, self.y, self.height)
