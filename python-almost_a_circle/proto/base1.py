b2612c644c50d6a80569e1fcd72b6f1196b78f2b
mokasp <kasperbouldin@gmail.com>
2023-10-02 01:53:54 -0500

    not sure if its formatting in right order

--- python-almost_a_circle/proto/base1.py
+++ python-almost_a_circle/proto/base1.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ module containing a class Base to create shapes """
-import json
 
 
 class Base:
@@ -23,8 +22,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """ returns the JSON string representation of a dict repr"""
-        return json.dumps(list_dictionaries)
