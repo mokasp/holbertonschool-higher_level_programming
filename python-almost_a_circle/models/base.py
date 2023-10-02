6314016501fb4acb729c048bd15fe59f422af3a3
mokasp <kasperbouldin@gmail.com>
2023-10-02 02:01:06 -0500

    it wasnt a string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -27,7 +27,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ returns the JSON string representation of a dict repr"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+        if list_dictionaries is None or len(list_dictionaries) != 0:
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
