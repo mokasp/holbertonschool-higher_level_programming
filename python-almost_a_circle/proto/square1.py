9984e48257e96752ed1f401e1928f1e794867c99
mokasp <kasperbouldin@gmail.com>
2023-10-01 23:18:15 -0500

    setter!

--- python-almost_a_circle/proto/square1.py
+++ python-almost_a_circle/proto/square1.py
@@ -38,26 +38,8 @@ class Square(Rectangle):
             identity of Square
         """
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     def __str__(self):
         """ returns string representation of Square """
         return "[{}] ({:d}) {:d}/{:d} - {:d}".format(
             self.__class__.__name__, self.id, self.x, self.y, self.height)
-
-    @property
-    def size(self):
-        """ method to get the current size of the Square """
-
-        return self.__width
-
-    @size.setter
-    def size(self, value):
-        """ method to set new size of the Square """
-
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width must be > 0")
-        else:
-            self.__width = value
