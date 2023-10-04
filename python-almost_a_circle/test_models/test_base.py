de5e95fa58baede3f07b01eef3d19bb644a77249
mokasp <kasperbouldin@gmail.com>
2023-10-04 14:26:10 -0500

    remove comments

--- python-almost_a_circle/test_models/test_base.py
+++ python-almost_a_circle/test_models/test_base.py
@@ -5,15 +5,12 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     def setUp(self):
-        print("\nRunning setUp method...")
         self.base_1 = Base()
         self.base_2 = Base()
         self.base_3 = Base(12)
     def tearDown(self):
-        print("Running tearDown method...")
+        pass
     def test_id(self):
-        print("Running Tests...")
         self.assertEqual(self.base_1.id, 1)
         self.assertEqual(self.base_2.id, 2)
         self.assertEqual(self.base_3.id, 12)
-
