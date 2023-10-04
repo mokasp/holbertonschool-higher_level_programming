6d51c82a8c50812db591fd412087e554379c3dd0
mokasp <kasperbouldin@gmail.com>
2023-10-04 18:55:59 -0500

    inital tests for Square

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -7,4 +7,40 @@ from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def tearDown(self):
+        pass
+
+    def test_createSquare1(self):
+        self.square_1 = Square(2)
+        self.assertEqual(self.square_1.id, 1)
+        self.assertEqual(self.square_1.size, 2)
+        self.assertEqual(self.square_1.x, 0)
+        self.assertEqual(self.square_1.y, 0)
+    
+    def test_createSquare2(self):
+        self.square_1 = Square(3, 1, 2, 12)
+        self.assertEqual(self.square_1.id, 12)
+        self.assertEqual(self.square_1.size, 3)
+        self.assertEqual(self.square_1.x, 1)
+        self.assertEqual(self.square_1.y, 2)
+
+    def test_createSquare3(self):
+        self.square_1 = Square(1, 1, 1, 1)
+        self.assertEqual(self.square_1.id, 1)
+        self.assertEqual(self.square_1.size, 1)
+        self.assertEqual(self.square_1.x, 1)
+        self.assertEqual(self.square_1.y, 1)
+        self.square_1.size = 2
+        self.assertEqual(self.square_1.size, 2)
+
+    def test_ConseqId(self):
+        self.square_1 = Square(2)
+        self.square_2 = Square(2)
+        self.square_3 = Square(2)
+        self.assertEqual(self.square_1.id, 1)
+        self.assertEqual(self.square_2.id, 2)
+        self.assertEqual(self.square_3.id, 3)
