a7a313c7a054be11d38489283b55c9836ebd7338
mokasp <kasperbouldin@gmail.com>
2023-10-04 16:58:19 -0500

    documentation for Base module

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,7 +5,34 @@ import os
 
 
 class Base:
-    """ a class representing a Base object to create shapes from """
+    """ a class representing a Base object to create shapes from
+
+    Class Attributes
+    ================
+    nb_objects - int
+        number of instances of Base
+
+    Instance Attributes
+    ===================
+    id - int
+        identity of object
+
+    Static Methods
+    --------------
+    to_json_string : JSON string
+        returns the JSON string representation of a dict repr
+    from_json_string :
+        converts JSON string to python object
+
+    Class Methods
+    -------------
+    save_to_file :
+        writes list of instances of Base to a file as its JSON represention
+    create :
+        returns an instance with all attributes already set
+    load_from_file :
+        returns a list of instance
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -15,8 +42,8 @@ class Base:
         .....
 
         parameters
-        ----------
-        id : int, optional
+        ==========
+        id - int, optional
             id of the object
         """
         if id is not None:
@@ -27,7 +54,13 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ returns the JSON string representation of a dict repr"""
+        """ returns the JSON string representation of a dict repr
+
+            parameters
+            ==========
+            list_dictionaries - list
+                list of dictionaries of attributes for object
+        """
         if list_dictionaries is None and len(list_dictionaries) != 0:
             return "[]"
         else:
@@ -38,11 +71,11 @@ class Base:
         """ function that writes a list of instances of Base to a text file \
             as its JSON represention
 
-        parameters
-        ==========
-        list_obj : list
-            list of instances of Base
-    """
+            parameters
+            ==========
+            list_obj - list
+                list of instances of Base
+        """
         filename = f"{cls.__name__}.json"
         new_list = []
         if list_objs is None:
@@ -58,12 +91,12 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        """ function that converts JSON string to object
+        """ function that converts JSON string to python object
 
-        parameters
-        ==========
-        json_string : JSON string
-            json string to convert to object
+            parameters
+            ==========
+            json_string - JSON string
+                json string to convert to python object
         """
         if json_string is None:
             return []
@@ -75,7 +108,7 @@ class Base:
 
             parameters
             ==========
-            dictionary : dict
+            dictionary - dict
                 dictionary containing attributes for new object
         """
         if cls.__name__ == "Square":
