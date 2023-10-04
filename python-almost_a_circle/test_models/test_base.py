c91848f7034f2fd127f8ffc4747922b9efc98f7a
mokasp <kasperbouldin@gmail.com>
2023-10-04 14:22:06 -0500

    test test

---
+++ python-almost_a_circle/test_models/test_base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" unittest for Base """
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    def setUp(self):
+        print("\nRunning setUp method...")
+        self.base_1 = Base()
+        self.base_2 = Base()
+        self.base_3 = Base(12)
+    def tearDown(self):
+        print("Running tearDown method...")
+    def test_id(self):
+        print("Running Tests...")
+        self.assertEqual(self.base_1.id, 1)
+        self.assertEqual(self.base_2.id, 2)
+        self.assertEqual(self.base_3.id, 12)
+
+if __name__=='__main__':
+	unittest.main()
