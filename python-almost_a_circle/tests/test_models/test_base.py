24349ff8cfbae2d64fde50c7fce7ff1e29ed6fa4
mokasp <kasperbouldin@gmail.com>
2023-10-04 16:37:24 -0500

    testing for id on base!

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,15 +2,40 @@
 """ unittest for Base """
 import unittest
 from models.base import Base
+import json
 
 class TestBase(unittest.TestCase):
+
     def setUp(self):
-        self.base_1 = Base()
-        self.base_2 = Base()
-        self.base_3 = Base(12)
+        Base._Base__nb_objects = 0
+
     def tearDown(self):
         pass
-    def test_id(self):
+
+    def test_Start(self):
+        self.base_1 = Base()
+        self.assertEqual(self.base_1.id, 1)
+
+    def test_Conseq(self):
+        self.base_1 = Base()
+        self.base_2 = Base()
+        self.base_3 = Base()
         self.assertEqual(self.base_1.id, 1)
         self.assertEqual(self.base_2.id, 2)
-        self.assertEqual(self.base_3.id, 12)
+        self.assertEqual(self.base_3.id, 3)
+
+    def test_Random(self):
+        self.base_1 = Base(4)
+        self.base_2 = Base(2)
+        self.base_3 = Base(8)
+        self.assertEqual(self.base_1.id, 4)
+        self.assertEqual(self.base_2.id, 2)
+        self.assertEqual(self.base_3.id, 8)
+
+    def test_ConseqAndRandom(self):
+        self.base_1 = Base()
+        self.base_2 = Base(50)
+        self.base_3 = Base()
+        self.assertEqual(self.base_1.id, 1)
+        self.assertEqual(self.base_2.id, 50)
+        self.assertEqual(self.base_3.id, 2)
