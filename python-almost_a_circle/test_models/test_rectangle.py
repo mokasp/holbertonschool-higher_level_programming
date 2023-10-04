c5eaecb81dd0d44d786da2671d31becb5d979ed7
mokasp <kasperbouldin@gmail.com>
2023-10-04 16:12:05 -0500

    all files

---
+++ python-almost_a_circle/test_models/test_rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" unittest for Rectangle """
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    pass
---
+++ python-almost_a_circle/test_models/test_square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" unittest for Square """
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    pass
