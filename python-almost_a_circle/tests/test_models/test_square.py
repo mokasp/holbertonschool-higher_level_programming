5ac22b1b36874974ff3c47bff2f27364406f0baf
mokasp <kasperbouldin@gmail.com>
2023-10-04 19:00:07 -0500

    documentaiton for square tests

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -7,14 +7,18 @@ from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
+    """ testing base """
 
     def setUp(self):
+        """ set up test """
         Base._Base__nb_objects = 0
 
     def tearDown(self):
+        """ tear down test """
         pass
 
     def test_createSquare1(self):
+        """ check simple square creation """
         self.square_1 = Square(2)
         self.assertEqual(self.square_1.id, 1)
         self.assertEqual(self.square_1.size, 2)
@@ -22,13 +26,15 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(self.square_1.y, 0)
     
     def test_createSquare2(self):
+        """ check more complex square creation """
         self.square_1 = Square(3, 1, 2, 12)
         self.assertEqual(self.square_1.id, 12)
         self.assertEqual(self.square_1.size, 3)
         self.assertEqual(self.square_1.x, 1)
         self.assertEqual(self.square_1.y, 2)
 
-    def test_createSquare3(self):
+    def test_SizeSetter(self):
+        """ check if setter sets"""
         self.square_1 = Square(1, 1, 1, 1)
         self.assertEqual(self.square_1.id, 1)
         self.assertEqual(self.square_1.size, 1)
@@ -38,6 +44,7 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(self.square_1.size, 2)
 
     def test_ConseqId(self):
+        """ consequative id """
         self.square_1 = Square(2)
         self.square_2 = Square(2)
         self.square_3 = Square(2)
