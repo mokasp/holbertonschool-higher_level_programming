9b4459c479f1c2ad316c036248ec9409c959b143
mokasp <kasperbouldin@gmail.com>
2023-10-20 19:29:35 -0500

    add database to create table in

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,7 @@
 -- SQL script that creates new database and table with a primary key
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `states` (
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
     id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (id)
+    PRIMARY KEY(id)
 );
