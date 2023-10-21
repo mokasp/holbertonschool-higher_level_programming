89c44537ca45296041a31cb31339284bfbee3d63
mokasp <kasperbouldin@gmail.com>
2023-10-20 19:15:56 -0500

    name typo

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- SQL script that creates new database and table with a primary key
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `states` (
+    id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
