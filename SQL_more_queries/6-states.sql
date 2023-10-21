a3e1ec360eb1321818d93fb3c243e4d2c8799d12
mokasp <kasperbouldin@gmail.com>
2023-10-20 19:16:56 -0500

    made id unique

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,5 +3,6 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `states` (
     id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    UNIQUE (id)
 );
