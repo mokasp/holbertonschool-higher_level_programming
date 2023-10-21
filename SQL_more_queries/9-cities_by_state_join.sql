d48b5cd308a1db450db9ff20b4cebf826f896112
mokasp <kasperbouldin@gmail.com>
2023-10-21 16:59:17 -0500

    first join (inner join)

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- SQL script that joins cities and states tables (inner join)
+SELECT cities.id, cities.name, states.name
+FROM cities
+    INNER JOIN states ON states.id = cities.state_id
+ORDER BY cities.id;
