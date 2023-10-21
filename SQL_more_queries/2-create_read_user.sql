efe4a9d4040d3148e4dec150b44dd83e7b8d7077
mokasp <kasperbouldin@gmail.com>
2023-10-20 19:00:38 -0500

    grant specific permission

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- SQL script that creates database and user and grants only select permission to user
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
