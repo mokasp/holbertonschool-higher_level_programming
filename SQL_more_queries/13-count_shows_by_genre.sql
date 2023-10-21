fd7e375f3d8b006686cb2e454789ac7c5aee0368
mokasp <kasperbouldin@gmail.com>
2023-10-21 18:34:09 -0500

    add order by statement

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,4 +2,5 @@
 SELECT DISTINCT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
     LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name;
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
