76f070f617cf112ac198b87ece0120cc4e819324
mokasp <kasperbouldin@gmail.com>
2023-10-21 21:15:38 -0500

    initial commit connecting to MySQLdb

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" script that lists all rows from SQL database"""
+import MySQLdb
+import sys
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=f"{sys.argv[1]}", passwd=f"{sys.argv[2]}", db=f"{sys.argv[3]}", charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
