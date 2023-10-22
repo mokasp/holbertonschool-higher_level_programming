11bee2cec51326818b02edaf539d13706f5eeff0
mokasp <kasperbouldin@gmail.com>
2023-10-22 16:30:05 -0500

    regexp, pattern matchin

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" script that lists all rows that start with N from SQL database """
+import MySQLdb
+import sys
+
+
+def main():
+    """ function that connects to SQL database and prints list of states"""
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=f"{sys.argv[1]}", passwd=f"{sys.argv[2]}",
+                           db=f"{sys.argv[3]}", charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^[n]' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
