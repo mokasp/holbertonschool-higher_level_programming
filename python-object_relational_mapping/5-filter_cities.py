876e6ec95848be23bef72e5adea09efa1ff1e306
mokasp <kasperbouldin@gmail.com>
2023-10-22 18:37:35 -0500

    add error handle

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -29,4 +29,7 @@ def main(un, pw, db_name, state_name):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    if len(sys.argv) == 5:
+        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    else:
+        pass
