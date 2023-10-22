e9f5a2128e5fe4321207202694b4ebd154248987
mokasp <kasperbouldin@gmail.com>
2023-10-21 19:04:53 -0500

    used inner join

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- SQL script that counts number of shows with a specific genre
-SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+SELECT DISTINCT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-    LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
+    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
