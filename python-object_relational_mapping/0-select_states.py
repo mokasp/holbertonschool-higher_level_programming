73f1b92c76f83920977520ab8f195e346f23bae5
mokasp <kasperbouldin@gmail.com>
2023-10-21 21:27:34 -0500

    pycode style

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,9 +3,12 @@
 import MySQLdb
 import sys
 
+
 def main():
     """ function that connects to SQL database and prints list of states"""
-    conn = MySQLdb.connect(host="localhost", port=3306, user=f"{sys.argv[1]}", passwd=f"{sys.argv[2]}", db=f"{sys.argv[3]}", charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=f"{sys.argv[1]}", passwd=f"{sys.argv[2]}",
+                           db=f"{sys.argv[3]}", charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
@@ -13,6 +16,7 @@ def main():
         print(row)
     cur.close()
     conn.close()
+
 
 if __name__ == "__main__":
     main()
