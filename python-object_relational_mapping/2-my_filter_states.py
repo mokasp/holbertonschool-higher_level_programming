bdeac52a3183684a3bef2f6ca71627b49d5c368c
mokasp <kasperbouldin@gmail.com>
2023-10-22 17:06:17 -0500

    taking user input in mysqldb query!

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" script that lists all rows that match user input from SQL database """
+import MySQLdb
+import sys
+
+
+def main(un, pw, db_name, state_name):
+    """ function that connects to SQL database and prints list \
+        of states that match user input"""
+    state_name = sys.argv[4]
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=f"{un}", passwd=f"{pw}",
+                           db=f"{db_name}", charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM states WHERE name = %s  \
+                ORDER BY id ASC", [state_name])
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
