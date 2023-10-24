3da2ad9fcc5fa3c3376275719925e2e1de5724b7
mokasp <kasperbouldin@gmail.com>
2023-10-24 10:40:53 -0500

    deleting entry!

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" module that connects to SQL server and deletes entries of states
+cointaing the letter a in table States"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import (sessionmaker)
+
+
+if __name__ == "__main__":
+    un = sys.argv[1]
+    pw = sys.argv[2]
+    dbn = sys.argv[3]
+    text = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(text.format(un, pw, dbn), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    the_states = session.query(State)
+    filtered = the_states.filter(State.name.contains("a"))
+    ordered = filtered.delete()
+    session.commit()
+    session.close()
