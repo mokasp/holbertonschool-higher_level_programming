b248128e09873fbf49951cf55dce822fc458e23e
mokasp <kasperbouldin@gmail.com>
2023-11-28 17:32:23 -0600

    check if int and semistandard

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 const x = process.argv[2];
-let row = []
-for (let i = 0; i < x; i++){ 
-    for (let j = 0; j < x; j++)
-        row.push('X')
+if (Number.isInteger(x)) {
+  const row = [];
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++) { row.push('X'); }
     if (i < x - 1) {
-        row.push('\n')
+      row.push('\n');
     }
-}
-if (x > 0) { 
-    console.log(row.join(""))
+  }
+  if (x > 0) {
+    console.log(row.join(''));
+  }
+} else {
+  console.log('Missing size');
 }
