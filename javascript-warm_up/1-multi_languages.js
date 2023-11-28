be7694008bcbec29df5b627773788ae09d24663f
mokasp <kasperbouldin@gmail.com>
2023-11-27 21:46:49 -0600

    print three lines

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const oneVar = 'C is fun';
+const twoVar = 'Python is cool';
+const threeVar = 'JavaScript is amazing';
+console.log(oneVar);
+console.log(twoVar);
+console.log(threeVar);
