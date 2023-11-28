f02c5b85b24c33ac8955a1253ca6e3ba3407d23f
mokasp <kasperbouldin@gmail.com>
2023-11-27 23:04:35 -0600

    removed oopsie command accidentalkly pasted

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const length = process.argv.length;
 if (length === 2) {
-  console.log('No argument');node -v
+  console.log('No argument');
 } else if (length === 3) {
   console.log('Argument found');
 } else {
