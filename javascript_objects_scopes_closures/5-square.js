851ae5328109dd436cb5090d571dd2fea2459cf2
mokasp <kasperbouldin@gmail.com>
2023-11-29 16:07:08 -0600

    11-29 | 4:07pm | semistandard validation

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle')
-  
+const Rectangle = require('./4-rectangle');
+
 class Square extends Rectangle {
-    constructor (size) {
-        super(size, size)
-    }
+  constructor (size) {
+    super(size, size);
   }
-  module.exports = Square;
+}
+module.exports = Square;
