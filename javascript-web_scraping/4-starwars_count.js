e8b4279aa71ec36a53bd121ee15d39e309150c9c
mokasp <kasperbouldin@gmail.com>
2023-12-01 20:33:11 -0600

    12-1 | 8:33pm | using new loop!

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,9 +2,15 @@
 const args = process.argv;
 args.splice(0, 2)
 const request = require('request')
-const url = `https://swapi-api.hbtn.io/api/people/${args[0]}`
+const url = `https://swapi-api.hbtn.io/api/films`
 
 request(url, function (error, response, body) {
     let parsedJson = JSON.parse(body);
-    console.log(parsedJson.films.length);
+    let filmList = []
+    for (let film of parsedJson.results) {
+        if (film.characters.includes("https://swapi-api.hbtn.io/api/people/18/")) {
+            filmList.push(film.title)
+        }
+    }
+    console.log(filmList.length);
 })
