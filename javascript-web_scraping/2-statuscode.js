2da7f9cf0a3d1ff26b8882615d16dd86284c7dae
mokasp <kasperbouldin@gmail.com>
2023-12-01 19:38:00 -0600

    12-1 | 7:38pm | add 'code'

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,5 @@ args.splice(0, 2)
 const https = require('https')
 
 https.get(args[0], response => {
-
-    console.log(response.statusCode)
-    })
+    console.log(`code: ${response.statusCode}`)
+})
