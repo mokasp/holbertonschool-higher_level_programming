a4eb684a30cf49a0a6b6347fc71138c2b530d466
mokasp <kasperbouldin@gmail.com>
2023-12-05 15:26:45 -0600

    12-5 | 3:26pm | looping through request body

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const args = process.argv;
+args.splice(0, 2)
+const request = require('request')
+const url = args[0]
+
+request(url, function (error, response, body) {
+    let parsedJson = JSON.parse(body);
+    let the_dict = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+    for (let entry of parsedJson) {
+        if (!(entry.userId in the_dict)) {
+            the_dict[entry.userId] = 0
+        }
+        if (entry.completed === true) {
+            the_dict[entry.userId] += 1
+        }   
+    }
+    console.log(the_dict);
+})
