eb0e89d9a8522c7651d8afc3e9ec31eda357d160
mokasp <kasperbouldin@gmail.com>
2023-12-05 15:28:59 -0600

    12-5 | 3:29pm | add extra conditon

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -9,7 +9,9 @@ request(url, function (error, response, body) {
     let the_dict = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
     for (let entry of parsedJson) {
         if (!(entry.userId in the_dict)) {
+            if (entry.completed === true) {
             the_dict[entry.userId] = 0
+            }
         }
         if (entry.completed === true) {
             the_dict[entry.userId] += 1
