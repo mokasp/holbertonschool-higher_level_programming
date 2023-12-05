d3205194a710522e6045c79aa2177e95a7813d20
mokasp <kasperbouldin@gmail.com>
2023-12-05 15:30:28 -0600

    12-5 | 3:30pm | using &&

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -8,10 +8,8 @@ request(url, function (error, response, body) {
     let parsedJson = JSON.parse(body);
     let the_dict = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
     for (let entry of parsedJson) {
-        if (!(entry.userId in the_dict)) {
-            if (entry.completed === true) {
+        if (!(entry.userId in the_dict) && (entry.completed === true)) {
             the_dict[entry.userId] = 0
-            }
         }
         if (entry.completed === true) {
             the_dict[entry.userId] += 1
