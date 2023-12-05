13c4882696b5bac81a74b0d116faeb56b2165bec
mokasp <kasperbouldin@gmail.com>
2023-12-05 17:56:32 -0600

    12-5 | 5:56pm | fixing error

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@ const args = process.argv;
 args.splice(0, 2);
 const request = require('request');
 
-request(args[0], (response) => {
+request(args[0], (error, response) => {
   console.log(`code: ${response.statusCode}`);
 });
