495cf6c55e67b2ad8b9d0e3975ec57771b0597ef
mokasp <kasperbouldin@gmail.com>
2023-12-05 18:11:06 -0600

    12-5 | 6:11pm | error handling

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,7 +4,7 @@ args.splice(0, 2);
 const request = require('request');
 const url = args[0];
 
-request(url, function (error, body) {
+request(url, function (error, response, body) {
   if (error) {
     console.log(error.stack);
   }
