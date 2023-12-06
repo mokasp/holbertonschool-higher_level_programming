d1471314611fcf4f958376d375f2b4f16a560f29
mokasp <kasperbouldin@gmail.com>
2023-12-05 18:00:52 -0600

    12-5 | 6:00pm | error handling

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,8 @@ args.splice(0, 2);
 const request = require('request');
 
 request(args[0], (error, response) => {
+  if (error) {
+    console.log(error.stack);
+  }
   console.log(`code: ${response.statusCode}`);
 });
